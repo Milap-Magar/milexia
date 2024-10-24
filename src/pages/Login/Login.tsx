281fe63e6cba2cd404eff213ace02781dcbdb3d0
milap-magar <milapmagar12@gmail.com>
2024-10-24 07:46:53 +0545

    Project Folder Management:

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import { account } from "../../service/appwrite"
+
+
+const Login = async() => {
+     const  handleLogin =  () => {
+
+    }
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
--- src/service/appwrite.ts
+++ src/service/appwrite.ts
@@ -1,11 +1,11 @@
 import { Client, Account } from "appwrite";
 
-const client = new Client();
+const client: Client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("67199e6d003806eb71b8");
 
-const account = new Account(client);
+const account: Account = new Account(client);
 
 export { account, client };
